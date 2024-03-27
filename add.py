3667c0166deaca4209ce44ca7f62a8ddc7279669
ZaherAlali001 <160915239+ZaherAlali001@users.noreply.github.com>
2024-03-27 18:40:38 +0300

    Create add.py
    add function

---
+++ add.py
@@ -0,0 +1,10 @@
+def add(request):
+    request_json = request.get_json()
+    x = request_json['X']
+    y = request_json['Y']
+    result = x + y
+    return {
+        "X": x,
+        "Y": y,
+        "Result": result
+    }
